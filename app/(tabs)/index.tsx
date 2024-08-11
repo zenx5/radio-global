f2fb7b36edd6405e30d70db46e0fa5d7e7662793
Octavio Martinez <omartinez1618@gmail.com>
2024-08-11 00:22:30 -0400

    fix

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,6 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { Player } from '@/components/Player';
 import { VerseDay } from '@/components/VerseDay';
-import Player2 from '@/components/Player2';
 
 export default function HomeScreen() {
   return (
